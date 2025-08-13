852cb390157aa1cbb34d2195079b25957ecbf483
victoriaplummer <victoria.plummer@webflow.com>
2025-08-13 11:44:03 -0400

    chore: fixed next build

--- betterauth-nextjs/src/lib/auth-client.ts
+++ betterauth-nextjs/src/lib/auth-client.ts
@@ -2,8 +2,17 @@ import { createAuthClient } from "better-auth/react";
 import config from "../../next.config";
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient({
-  baseURL:
-    typeof window !== "undefined"
-      ? `${window.location.origin}${config.assetPrefix}/api/auth`
-      : `http://localhost:8787${config.assetPrefix}/api/auth`,
+  baseURL: (() => {
+    const assetPrefix = config.assetPrefix || "";
+
+    if (typeof window !== "undefined") {
+      return `${window.location.origin}${assetPrefix}/api/auth`;
+    }
+
+    if (assetPrefix.startsWith("http")) {
+      return `${assetPrefix}/api/auth`;
+    }
+
+    return `https://hello-webflow-cloud.webflow.io${assetPrefix}/api/auth`;
+  })(),
 });
